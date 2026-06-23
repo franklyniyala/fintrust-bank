45650cae32cd08b07745ee83ce167e5afb3c2ab2
Franklyn <franklyniyala@gmail.com>
2026-06-23 19:10:51 +0100

    Made corrections on the infrastructure to remove unneccessary resources

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "fintrust_instance" {
   ami                         = "ami-0521cb2d60cfbb1a6"
   instance_type               = "t3.medium"
-  subnet_id                   = aws_subnet.fintrust_subnet.id
+  subnet_id                   = aws_subnet.fintrust_public_subnet.id
   vpc_security_group_ids      = [aws_security_group.fintrust_sg.id]
   key_name                    = "franklyn"
   associate_public_ip_address = true
